ebe7072f65c7fe328c6cbccb0148381877c29ba4
Nubia Yesenia <nubiayeseniacamposortega440@gmail.com>
2026-03-21 17:22:11 +0000

    Hice una listas con ANGULAR

--- MiPrimerAngular/src/app/app.ts
+++ MiPrimerAngular/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MiPrimerAngular');
+ Bienvenidos = 'Hola nubia';
+
+ Estudiantes = [ 'cuadernos','Gorra', 'Gafas',]
 }
+
+
