316e3eb2a16466ce442d36c4b82c2c00a3b81f2b
Nubia Yesenia <nubiayeseniacamposortega440@gmail.com>
2026-03-26 02:03:38 +0000

    Creacion de paginas

--- MiPrimerAngular/src/app/app.routes.ts
+++ MiPrimerAngular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {} from './pages/inicio/inicio';
+import {} from './pages/contacto/contacto';
+import {} from './pages/ejercicios/ejercicios';
 
 export const routes: Routes = [];
+
+ //
